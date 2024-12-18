cde39155f9a1fe1518eaedd28c2dd5d50cc15c09
dev-guilhCASTRO <guilherme.cbpereira@gmail.com>
2024-12-17 22:46:11 -0300

    Added test route

--- CreateContact/CreateContact.Api/Controllers/Contacts/CreateContactsController.cs
+++ CreateContact/CreateContact.Api/Controllers/Contacts/CreateContactsController.cs
@@ -33,5 +33,22 @@ namespace CreateContact.Api.Controllers.Contacts
                 throw;
             }
         }
+
+        [HttpPost("test")]
+        [SwaggerResponse(StatusCodes.Status201Created)]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, type: typeof(BaseReponse))]
+        public async Task<IActionResult> CreateAsync2(
+            [FromBody] CreateContactRequest request)
+        {
+            try
+            {
+                return Created(string.Empty, await _mediator.Send(request));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
     }
 }
