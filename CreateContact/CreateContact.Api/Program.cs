4da4f99905e7a71f2f55c1eb022c72d23cd575c0
dev-guilhCASTRO <guilherme.cbpereira@gmail.com>
2025-02-16 14:22:13 -0300

    Added builder as arg

--- CreateContact/CreateContact.Api/Program.cs
+++ CreateContact/CreateContact.Api/Program.cs
@@ -1,13 +1,14 @@
 using CreateContact.Api;
 
+
 internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
         var startup = new Startup(builder.Configuration);
-        startup.ConfigureServiceImpl(builder.Services);
+        startup.ConfigureServiceImpl(builder);
 
         var app = builder.Build();
         startup.ConfigureImpl(app, app.Environment);
